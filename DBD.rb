acada3e64ca219b216836eea1d097d9a92d47f45
anthonytobar <37843535+anthonytobar@users.noreply.github.com>
2020-01-19 00:34:06 -0800

    Add files via upload

---
+++ DBD.rb
@@ -0,0 +1,71 @@
+use_bpm 65
+use_synth :piano
+2.times do
+  play :e1
+  sleep 1
+  play :e1
+  sleep 1
+  play :e1
+  sleep 1
+  play :e1
+  play :b1
+end
+
+2.times do
+  play :g1
+  sleep 1
+  play :g1
+  sleep 1
+  play :g1
+  sleep 1
+  play :g1
+  play :e1
+end
+
+4.times do
+  play :f1
+  play :b1
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :b1
+  play :b2
+  play :d1
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :b1
+  play :b2
+  play :c1
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :b1
+  play :b2
+  play :d1
+  sleep 0.5
+  play :b2
+  play :c1
+  sleep 0.5
+  play :b1
+  play :b2
+  play :c1
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :b1
+  play :b2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :b1
+  play :b2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :b1
+  play :b2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+end
